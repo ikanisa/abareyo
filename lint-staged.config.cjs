468ee9946b65f757ecb37440a14a244294f21219
ikanisa <152620141+ikanisa@users.noreply.github.com>
2025-11-02 14:48:44 +0100

    Fix lint-staged globs for nested files

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [
+  '**/*.{js,jsx,ts,tsx}': [
     'eslint --max-warnings=0',
     'prettier --write'
   ],
-  '*.{css,scss,md,mdx,json,yaml,yml}': 'prettier --write'
+  '**/*.{css,scss,md,mdx,json,yaml,yml}': 'prettier --write'
 };
